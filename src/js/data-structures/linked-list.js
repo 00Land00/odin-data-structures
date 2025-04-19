d00fcd13d61330c812b2e6d311ef7e9d8caa7bdc
Lander Joshua Vitug <lander.vitug@gmail.com>
2025-04-19 02:54:55 -0500

    Refactor linkedlist to be doubly linked and add more functions

--- src/js/data-structures/linked-list.js
+++ src/js/data-structures/linked-list.js
@@ -8,30 +8,39 @@ function LinkedList() {
   function append(value) {
     const newNode = Node(value);
     newNode.next = null;
-    if (headNode === null) {
-      headNode = newNode;
-    }
-    if (tailNode !== null) {
+    newNode.prev = null;
+
+    if (!headNode) {
+      headNode = tailNode = newNode;
+    } else {
       tailNode.next = newNode;
+      newNode.prev = tailNode;
+      tailNode = newNode;
     }
-    tailNode = newNode;
     nodeSize++;
   }
 
   function prepend(value) {
     const newNode = Node(value);
-    if (headNode !== null) {
+    newNode.next = null;
+    newNode.prev = null;
+
+    if (!headNode) {
+      headNode = tailNode = newNode;
+    } else {
       newNode.next = headNode;
+      headNode.prev = newNode;
+      headNode = newNode;
     }
-    if (tailNode === null) {
-      tailNode = newNode;
-    }
-    headNode = newNode;
     nodeSize++;
   }
 
   function size() {
     return nodeSize;
+  }
+
+  function isEmpty() {
+    return nodeSize === 0;
   }
 
   function head() {
@@ -47,57 +56,108 @@ function LinkedList() {
       return null;
     }
 
-    let curNode = headNode;
-    for (let i = 0; i < index; i++) {
-      curNode = curNode.next;
+    let curNode, i;
+    if (index <= nodeSize / 2) {
+      curNode = headNode;
+      for (i = 0; i < index; i++) {
+        curNode = curNode.next;
+      }
+    } else {
+      curNode = tailNode;
+      for (i = nodeSize - 1; i > index; i--) {
+        curNode = curNode.prev;
+      }
     }
+
     return curNode;
+  }
+
+  function shift() {
+    if (nodeSize === 0) {
+      return null;
+    }
+
+    const shiftedNode = headNode;
+
+    if (nodeSize === 1) {
+      headNode = tailNode = null;
+    } else {
+      headNode = headNode.next;
+      headNode.prev = null;
+    }
+
+    shiftedNode.next = null;
+
+    nodeSize--;
+    return shiftedNode;
   }
 
   function pop() {
     if (nodeSize === 0) {
       return null;
     }
+
+    const poppedNode = tailNode;
+
     if (nodeSize === 1) {
-      const poppedNode = headNode;
       headNode = tailNode = null;
-      nodeSize = 0;
-      return poppedNode;
+    } else {
+      tailNode = tailNode.prev;
+      tailNode.next = null;
     }
 
-    let curNode = headNode;
-    for (let i = 0; i < nodeSize - 2; i++) {
-      curNode = curNode.next;
-    }
-    const poppedNode = curNode.next;
-    curNode.next = null;
-    tailNode = curNode;
+    poppedNode.prev = null;
+
     nodeSize--;
     return poppedNode;
   }
 
   function contains(value) {
-    let curNode = headNode;
-    while (curNode !== null) {
-      if (curNode.value === value) {
-        return true;
-      }
-      curNode = curNode.next;
+    let headPtr = headNode;
+    let tailPtr = tailNode;
+
+    if (!headPtr || !tailPtr) {
+      return false;
     }
+
+    while (headPtr && tailPtr) {
+      if (headPtr.value === value || tailPtr.value === value) return true;
+      if (headPtr === tailPtr || headPtr.next === tailPtr) break;
+
+      headPtr = headPtr.next;
+      tailPtr = tailPtr.prev;
+    }
+
     return false;
   }
 
   function find(arg) {
-    let curNode = headNode;
-    let i = 0;
+    let headPtr = headNode;
+    let headIndex = 0;
+    let tailPtr = tailNode;
+    let tailIndex = nodeSize - 1;
+
+    if (!headPtr || !tailPtr) {
+      return -1;
+    }
 
     const isPredicate = typeof arg === "function";
-    while (curNode !== null) {
-      const match = isPredicate ? arg(curNode.value) : curNode.value === arg; 
+    while (headPtr && tailPtr) {
+      const headMatch = isPredicate
+        ? arg(headPtr.value)
+        : headPtr.value === arg;
+      const tailMatch = isPredicate
+        ? arg(tailPtr.value)
+        : tailPtr.value === arg;
 
-      if (match) return i;
-      curNode = curNode.next;
-      i++;
+      if (headMatch) return headIndex;
+      if (tailMatch) return tailIndex;
+      if (headPtr === tailPtr || headPtr.next === tailPtr) break;
+
+      headPtr = headPtr.next;
+      headIndex++;
+      tailPtr = tailPtr.prev;
+      tailIndex--;
     }
 
     return -1;
@@ -111,13 +171,14 @@ function LinkedList() {
       curNode = curNode.next;
     }
     outputStr.push(`null`);
-    return outputStr.join(` -> `);
+    return outputStr.join(` <-> `);
   }
 
   function insertAt(value, index) {
     if (index < 0 || index > nodeSize) {
       return;
     }
+
     if (index === 0) {
       prepend(value);
       return;
@@ -128,12 +189,27 @@ function LinkedList() {
     }
 
     const newNode = Node(value);
-    let curNode = headNode;
-    for (let i = 0; i < index - 1; i++) {
-      curNode = curNode.next;
+    let curNode;
+    if (index <= nodeSize / 2) {
+      curNode = headNode;
+      for (let i = 0; i < index - 1; i++) {
+        curNode = curNode.next;
+      }
+      newNode.next = curNode.next;
+      newNode.prev = curNode;
+      newNode.next.prev = newNode;
+      curNode.next = newNode;
+    } else {
+      curNode = tailNode;
+      for (let i = nodeSize - 1; i > index; i--) {
+        curNode = curNode.prev;
+      }
+      newNode.prev = curNode.prev;
+      newNode.next = curNode;
+      newNode.prev.next = newNode;
+      curNode.prev = newNode;
     }
-    newNode.next = curNode.next;
-    curNode.next = newNode;
+
     nodeSize++;
   }
 
@@ -142,11 +218,7 @@ function LinkedList() {
       return;
     }
     if (index === 0) {
-      headNode = headNode.next;
-      if (!headNode) {
-        tailNode = null;
-      }
-      nodeSize--;
+      shift();
       return;
     }
     if (index === nodeSize - 1) {
@@ -154,27 +226,48 @@ function LinkedList() {
       return;
     }
 
-    let curNode = headNode;
-    for (let i = 0; i < index - 1; i++) {
-      curNode = curNode.next;
+    let curNode;
+    if (index <= nodeSize / 2) {
+      curNode = headNode;
+      for (let i = 0; i < index; i++) {
+        curNode = curNode.next;
+      }
+    } else {
+      curNode = tailNode;
+      for (let i = nodeSize - 1; i > index; i--) {
+        curNode = curNode.prev;
+      }
     }
-    curNode.next = curNode.next.next;
+
+    curNode.prev.next = curNode.next;
+    curNode.next.prev = curNode.prev;
+    curNode.next = curNode.prev = null;
+
     nodeSize--;
+  }
+
+  function clear() {
+    headNode = null;
+    tailNode = null;
+    nodeSize = 0;
   }
 
   return {
     append,
     prepend,
     size,
+    isEmpty,
     head,
     tail,
     at,
+    shift,
     pop,
     contains,
     find,
     toString,
     insertAt,
     removeAt,
+    clear,
   };
 }
 
